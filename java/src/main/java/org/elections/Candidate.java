0198492f2ad59aaf0494d46bd8c2f00b2c5d260a
nergal-perm <nergal@psu.ru>
2022-05-31 08:30:50 +0500

    primitive-obsession: store candidates in a dedicated collection class
    Следующий шаг - инкапсуляция списка кандидатов в выделенном классе.
    Полностью перейти на использование нового объекта не получилось, в
    этом коммите реализован отказ от использования отдельного списка
    официально зарегистрированных кандидатов. Вместо него все обращения
    идут к новому классу-коллекции Candidates.

---
+++ java/src/main/java/org/elections/Candidate.java
@@ -0,0 +1,27 @@
+package org.elections;
+
+public class Candidate {
+    private final String name;
+    private final boolean isOfficial;
+
+    public Candidate(String name, boolean isOfficial) {
+        this.name = name;
+        this.isOfficial = isOfficial;
+    }
+
+    public static Candidate official(String name) {
+        return new Candidate(name, true);
+    }
+
+    public static Candidate unofficial(String name) {
+        return new Candidate(name, false);
+    }
+
+    public String name() {
+        return this.name;
+    }
+
+    public boolean isOfficial() {
+        return this.isOfficial;
+    }
+}
